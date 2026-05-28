ff2dd057fed437c73db9d8c1d93a354365f38c1c
SupremeHo <suho9813@gmail.com>
2026-05-28 15:00:14 +0900

    LLM의 답변에 대한 JSON 구조화. 연구 및 수정 필요.

--- resources/Storage_JSON.py
+++ resources/Storage_JSON.py
@@ -1,6 +1,29 @@
 # Module for outputting the user's questions, LLM's prompts, and metadata as JSON.
 
 import json
+from openai import OpenAI
 from datetime import datetime
 
+
+LLM_response = {
+    "request_id": "2026-05-27T12-45-30",
+    "created_at": "2026-05-27T12:45:30+09:00",
+    "provider": "openai",
+    "model": "gpt-4o-mini-2024-07-18",
+    "user_prompt": "Who are you?",
+    "assistant_response": "...",
+    "usage": {
+    "prompt_tokens": 25,
+    "completion_tokens": 70,
+    "total_tokens": 95
+    },
+    "cost": {
+        "input_usd": 0.00000375,
+        "output_usd": 0.000042,
+        "total_usd": 0.00004575
+    },
+    "finish_reason": "stop",
+    "success": true, # type: ignore
+    "error": null # type: ignore
+}
 
