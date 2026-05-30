83b527ea1770e70e081df72df18e07497a155928
SupremeHo <suho9813@gmail.com>
2026-05-30 16:18:48 +0900

    응답 생성 및 jsonl 파일 생성 기능 추가.

--- resources/CLI.py
+++ resources/CLI.py
@@ -1,15 +1,23 @@
 ﻿# CLI module
 # In charge of Typer command line.
 
+from datetime import datetime, timezone
+from uuid import uuid4
 import time
+
 import typer
 from rich.progress import track
-from openai import OpenAI
+
 import env_check
 import llm_client
-import list_models
+from list_models import list_available_models
+from llm_client import ask_gpt
+from storage_json import append_jsonl
+from schemas import LLMCallLog
+
 
 app = typer.Typer()
+
 
 # Load environment variables from .env file with the checking function defined in env_check.py.
 @app.command()
@@ -19,20 +27,44 @@ def check_env():
 # Defining the ask command. Prompt the user for a question and output the question.
 @app.command()
 def ask(system_prompt: str, user_question: str):
-    typer.echo(f"System prompt: {system_prompt}")
-    typer.echo(f"Your question: {user_question}")
+    run_id = str(uuid4())
+    created_at = datetime.now(timezone.utc)
 
-    # Simulate a loading process while the LLM generates a response (this is just for demonstration and can be removed or replaced with actual progress tracking).
-    # for step in track(range(100), description="\nResponse generating..."):
-    #     time.sleep(1)
-    # print("Response generated.\n")
+    try:
+        result = ask_gpt(system_prompt, user_question)
 
-    llm_client.ask_llm(system_prompt, user_question)
+        log = LLMCallLog(
+            run_id = run_id,
+            created_at = created_at,
+            user_prompt = user_question,
+            result = result,
+            success = True,
+            error = None,
+            elapsed_ms = None
+        )
+
+        typer.echo(f"OpenAI GPT's Response:\n{result.response_text}\n")
+
+    except Exception as e:
+        log = LLMCallLog(
+            run_id = run_id,
+            created_at = created_at,
+            user_prompt = user_question,
+            result = None,
+            success = False,
+            error = str(e),
+            elapsed_ms = None
+        )
+
+        typer.echo(f"An error occurred: {e}")
+    
+    append_jsonl("logs/GPT_call_logs.jsonl", log)
+
 
 # Defining the list-models command. This will list available models from the OpenAI API.
 @app.command()
 def list_models():
-    list_models.list_available_models(OpenAI())
+    list_available_models(llm_client.client)
 
 # Not yet implemented. This command will show the user's history of questions and LLM responses.
 @app.command()
