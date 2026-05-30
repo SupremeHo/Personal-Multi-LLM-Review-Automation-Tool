9bb794f6b1caa457ceec975ced063b62d47d34dc
SupremeHo <suho9813@gmail.com>
2026-05-30 13:24:02 +0900

    진행도 표시 추가. 파일 이름 수정.

--- resources/CLI.py
+++ resources/CLI.py
@@ -1,30 +1,38 @@
 ﻿# CLI module
 # In charge of Typer command line.
 
+import time
 import typer
+from rich.progress import track
 from openai import OpenAI
-from env_check import check_environment_variables
-import LLM_Client
-import List_Models
+import env_check
+import llm_client
+import list_models
 
 app = typer.Typer()
 
 # Load environment variables from .env file with the checking function defined in env_check.py.
 @app.command()
 def check_env():
-    check_environment_variables()
+    env_check.check_environment_variables()
 
 # Defining the ask command. Prompt the user for a question and output the question.
 @app.command()
 def ask(system_prompt: str, user_question: str):
     typer.echo(f"System prompt: {system_prompt}")
     typer.echo(f"Your question: {user_question}")
-    LLM_Client.ask_llm(system_prompt, user_question)
-    
+
+    # Simulate a loading process while the LLM generates a response (this is just for demonstration and can be removed or replaced with actual progress tracking).
+    # for step in track(range(100), description="\nResponse generating..."):
+    #     time.sleep(1)
+    # print("Response generated.\n")
+
+    llm_client.ask_llm(system_prompt, user_question)
+
 # Defining the list-models command. This will list available models from the OpenAI API.
 @app.command()
 def list_models():
-    List_Models.list_available_models(OpenAI())
+    list_models.list_available_models(OpenAI())
 
 # Not yet implemented. This command will show the user's history of questions and LLM responses.
 @app.command()
