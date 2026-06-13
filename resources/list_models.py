7e5a80b5c944c10d4b26986f740be2fbe95d5d93
SupremeHo <suho9813@gmail.com>
2026-06-13 14:57:53 +0900

    Improve model list selection: change user input method and add exception handling

--- resources/list_models.py
+++ resources/list_models.py
@@ -1,33 +1,51 @@
 # Module for listing all available models.
 # You can choose to list all models or skip this step based on user input.
 
+import sys
+
 from anthropic import Anthropic
 from google import genai
 from openai import OpenAI
 
 
 def list_available_models(client_openai: OpenAI, client_anthropic: Anthropic, client_google: genai):
-    print("Would you like to list the available models in OpenAI, Anthropic, and Google?")
-    answer = input(
-        "If you input the word 'all', list the all available models. If you want to skip, input the word 'No': "
-    ).lower()
-    if answer == "openai":
-        list_available_openai_models(client_openai)
-    elif answer == "anthropic":
-        list_available_claude_models(client_anthropic)
-    elif answer == "google":
-        list_available_claude_models(client_google)
-    elif answer == "all":
-        list_available_openai_models(client_openai)
-        list_available_claude_models(client_anthropic)
-        list_available_gemini_models(client_google)
-    elif answer == "no":
-        print("\nSkipping model listing...\n")
-    else:
-        print("\nPlease check your input.\n")
+    """"""
+    while True:
+        try:
+            print(
+                "Would you like to list the available API's models? Please select and enter one of the three companies. (OpenAI, Anthropic, or Google)"
+            )
+            answer = input(
+                "If you want to check the all available models, input the word 'Yes'. If you want to skip, input the word 'No': "
+            ).lower()
+
+            if answer == "openai":
+                list_available_openai_models(client_openai)
+                break
+            elif answer == "anthropic":
+                list_available_claude_models(client_anthropic)
+                break
+            elif answer == "google":
+                list_available_gemini_models(client_google)
+                break
+            elif answer == "yes":
+                list_available_openai_models(client_openai)
+                list_available_claude_models(client_anthropic)
+                list_available_gemini_models(client_google)
+                break
+            elif answer == "no":
+                print("\nSkipping model listing...\n")
+                break
+            else:
+                print("\nError. Please enter your input correctly.\n")
+                continue
+        except ValueError as e:
+            sys.exit(e + " Invalid value input. Exit the program.")
 
 
 def list_available_openai_models(client=OpenAI()):
+    """The function listing the available models in OpenAI."""
+
     try:
         models = client.models.list()
         print("\n" + "======== Available OpenAI's Models ========")
@@ -40,6 +58,8 @@ def list_available_openai_models(client=OpenAI()):
 
 
 def list_available_claude_models(client=Anthropic()):
+    """The function listing the available models in Anthropic."""
+
     try:
         models = client.models.list()
         print("\n" + "======== Available Anthropic's Models ========")
@@ -51,19 +71,23 @@ def list_available_claude_models(client=Anthropic()):
 
 
 def list_available_gemini_models(client=genai.Client()):
+    """The function listing the available models in Google Gemini."""
+
     try:
         models = client.models.list()
         print("\n" + "======== Available Google's Models ========")
         for model in models:
             if "gemini" in model.name:
-                print(model.name)
+                text = model.name
+                new_text = text.strip("models/")
+                print(new_text)
         print("======== End of Google's Models ========\n")
     except Exception as e:
         print(e)
 
 
-if __name__ == "__main__":
-    client_openai = OpenAI()
-    client_anthropic = Anthropic()
-    client_gemini = genai.Client()
-    list_available_models(client_openai, client_anthropic, client_gemini)
+# if __name__ == "__main__":
+#     client_openai = OpenAI()
+#     client_anthropic = Anthropic()
+#     client_gemini = genai.Client()
+#     list_available_models(client_openai, client_anthropic, client_gemini)
